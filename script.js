7d09d3fadb3b0a37d770c7aa0296ffa3799ec08c
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2024-02-05 18:04:59 -0800

    Shenanegans

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendRequest(buttonId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://your-api-endpoint.com?button=" + buttonId, true);
+    xhttp.send();
+}
